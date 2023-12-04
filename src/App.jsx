e69253ec817e481474bf61df0678b6f9934cba32
louraknohaila <140641356+louraknohaila@users.noreply.github.com>
2023-12-05 00:40:34 +0100

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -11,7 +11,7 @@ function App() {
   return(
     <BrowserRouter>
        <Routes>
-          <Route path="/" element={<Start/>} />
+          <Route path="/SQL_QCM" element={<Start/>} />
           <Route path="/QCM" element={<QCM/>}/>
           <Route path="/Cours" element={<Cours />}/>
        </Routes>
